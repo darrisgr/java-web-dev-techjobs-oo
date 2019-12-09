57022bdf35952bdc5153753bec28aa8f8ba168b2
Gerard Darris <darrisgr@gmail.com>
2019-12-09 08:21:43 -0600

    Added JUnit 4.12 and updated JobTest.java

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,4 +1,9 @@
 package org.launchcode.techjobs_oo.Tests;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.launchcode.techjobs_oo.Job;
+
 public class JobTest {
 }
