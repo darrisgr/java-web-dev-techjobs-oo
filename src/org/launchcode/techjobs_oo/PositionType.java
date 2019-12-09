b780033658043674f4d701a07889009e05ebf21b
Gerard Darris <darrisgr@gmail.com>
2019-12-09 05:38:12 -0600

    Updated PositionType.java

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -28,7 +28,7 @@ public class PositionType {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof PositionType)) return false;
         PositionType positionType = (PositionType) o;
         return getId() == positionType.getId();
     }
