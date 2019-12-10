a23cc038dab2dcb55667a65d1507106b7132f5a6
Gerard Darris <darrisgr@gmail.com>
2019-12-09 18:39:48 -0600

    Updated JobTest.java

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -3,7 +3,6 @@ package org.launchcode.techjobs_oo.Tests;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 import org.launchcode.techjobs_oo.*;
 
 public class JobTest {
