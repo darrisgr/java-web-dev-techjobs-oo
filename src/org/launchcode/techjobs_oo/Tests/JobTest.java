0b588d58cd509cdd74f5de5cbf892741619cfc10
Gerard Darris <darrisgr@gmail.com>
2019-12-09 22:12:05 -0600

    Updated Job.java, Main.java, JobTest.java

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -11,6 +11,7 @@ public class JobTest {
     Job constructor_test_job;
     Job equality_job1;
     Job equality_job2;
+    Job toString_job;
 
     @Before
     public void createJobObjects() {
@@ -22,6 +23,8 @@ public class JobTest {
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
         equality_job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        toString_job = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"),
+                new CoreCompetency("Taste"));
     }
 
     @Test //Test that the ID values for both test objects are unique and differ by 1
@@ -43,6 +46,49 @@ public class JobTest {
     @Test //Test that two Job objects are not equal despite having same constructor values
     public void testJobsForEquality() {
         assertFalse(equality_job1.equals(equality_job2));
+    }
+
+    @Test //Test that when passed a Job object, returns a string w/blank line before and after job information
+    public void testJobReturnsStringWithBlankLines() {
+        char[] toStringChars = toString_job.toString().toCharArray();
+        int lastChar = toStringChars.length - 1;
+
+        assertEquals("\n", Character.toString(toStringChars[0]));
+        assertEquals("\n", Character.toString(toStringChars[lastChar]));
+    }
+
+    @Test //Test that string should contain label for each field, followed by data stored in that field
+    public void testJobContainsLabelsAndDataOnOwnLines() {
+
+        assertTrue(toString_job.toString().contains("ID: "));
+        assertEquals(6, toString_job.getId());
+
+        assertTrue(toString_job.toString().contains("Name: "));
+        assertEquals("Ice cream taster", toString_job.getName());
+
+        assertTrue(toString_job.toString().contains("Employer: "));
+        assertEquals("", toString_job.getEmployer().toString());
+
+        assertTrue(toString_job.toString().contains("Location: "));
+        assertEquals("Home", toString_job.getLocation().toString());
+
+        assertTrue(toString_job.toString().contains("Position Type: "));
+        assertEquals("UX", toString_job.getPositionType().toString());
+
+        assertTrue(toString_job.toString().contains("Core Competency: "));
+        assertEquals("Taste", toString_job.getCoreCompetency().toString());
+    }
+
+    @Test //Test that if a field is empty, inputs "Data not available." after label
+    public void testJobAccountsForEmptyFields() {
+        assertTrue(toString_job.toString().contains("Employer: Data not available."));
+        assertFalse(toString_job.toString().contains("Name: Data not available."));
+    }
+
+    //Bonus
+    @Test //Test for if a job ONLY contains Id field, returns "OOPS! This job does not seem to exist."
+    public void testJobAccountsForAllEmptyFields() {
+        assertEquals("OOPS! This job does not seem to exist.", test_job1.toString());
     }
 
 }
